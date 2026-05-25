4fa2710d16e4d8e51fd1b3530ddad85b8a6ddd90
Antônio Rocha da Silva <antoniorochadasilvaa@gmail.com>
2026-05-25 11:05:47 -0300

    Feat: ap1.dart realizada

---
+++ ap1_variaveis/ap1.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main(){
+
+  bool ativo = true;
+  double peso = 70;
+  int idade = 18;
+  String nome = "Antônio", sobrenome = "Rocha da Silva";
+  String? nacionalidade; // = "Brasileiro"
+
+  print("Nome completo: $nome $sobrenome");
+  print("$idade ${idade >= 18 ? "Maior de idade" : "Menor de idade"}");
+  print("Situação: ${ativo == true ? "Ativo" : "Inativo"}");
+  print("Peso: $peso");
+  print("Nacionalidade: ${nacionalidade != null ? nacionalidade : "Nao informada"}");
+}
