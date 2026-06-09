d55598792c6d7579b176b9e8d1ea1efe47570f6e
paozinho6254 <antoniorochadasilvaa@gmail.com>
2026-06-09 14:20:46 -0300

    Feat: adicao da atividade 1 de ap3_funcoes

---
+++ ap3_funcoes/ap1.dart
@@ -0,0 +1,39 @@
+import 'dart:math';
+
+final aleatorio = Random();
+
+void main(){
+  List <int> lista1 = List.generate(5, (_) => aleatorio.nextInt(100)+1);
+  List <int> lista2 = List.generate(5, (_) => aleatorio.nextInt(100)+1);
+  
+  imprimeLista(lista1);
+  imprimeLista(lista2);
+
+  List <int> lista3 = somaLista(lista1, lista2);
+
+  imprimeLista(lista3);
+}
+
+void imprimeLista(List<int> lista){
+  if(lista.isEmpty){
+    print("Lista vazia");
+  }
+  else{
+    print("Lista: ${lista.join(', ')}");
+  }
+}
+
+List<int> somaLista(List<int> lista1, List<int> lista2){
+  if(lista1.length != lista2.length){
+    return [];
+  }
+
+  List<int> lista3 = [];
+
+  for(int i = 0; i < lista1.length; i++){
+    print("${lista1[i]}+${lista2[i]}");
+    lista3.add(lista1[i] + lista2[i]);
+  }
+  return lista3;
+}
+
