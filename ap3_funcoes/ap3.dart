0157b009175676af8f9fc48d6f83e40815a48df3
paozinho6254 <antoniorochadasilvaa@gmail.com>
2026-06-10 15:52:50 -0300

    Feat: adicao da atividade 4 de ap3_funcoes

--- ap3_funcoes/ap3.dart
+++ ap3_funcoes/ap3.dart
@@ -4,7 +4,7 @@ const PI = 3.14;
 final aleatorio = Random();
 
 void main() {
-  final raios = List.generate(10, (_) => aleatorio.nextInt(100) + 1);
+  final raios = List.generate(10, (_) => aleatorio.nextDouble() * 100 + 1);
 
   for (var raio in raios) {
     print(
@@ -13,10 +13,10 @@ void main() {
   }
 }
 
-double calcAreaCirculo(int raio) {
+double calcAreaCirculo(double raio) {
   return (raio * raio) * PI;
 }
 
-double calcPerimetroCirculo(int raio) {
+double calcPerimetroCirculo(double raio) {
   return 2 * raio * PI;
 }
---
+++ ap3_funcoes/ap4.dart
@@ -0,0 +1,59 @@
+import 'dart:math';
+
+final hexMap = {10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'};
+final aleatorio = Random();
+
+void main() {
+  final temperaturas = List.generate(15, (_) => aleatorio.nextInt(5000) + 1);
+
+  imprimirConversoes(temperaturas);
+}
+
+String convertBinario(int temperatura) {
+  String binario = '';
+  do {
+    binario = (temperatura % 2).toString() + binario;
+    temperatura ~/= 2;
+  } while (temperatura > 0);
+
+  return binario;
+}
+
+String convertOctal(int temperatura) {
+  String octal = '';
+
+  do {
+    octal = (temperatura % 8).toString() + octal;
+    temperatura ~/= 8;
+  } while (temperatura > 0);
+
+  return octal;
+}
+
+String convertHexadecimal(int temperatura) {
+  String hexadecimal = '';
+  do {
+    int resto = temperatura % 16;
+    if (resto > 9) {
+      hexadecimal = hexMap[resto]! + hexadecimal;
+    } else {
+      hexadecimal = (resto).toString() + hexadecimal;
+    }
+    temperatura ~/= 16;
+  } while (temperatura > 0);
+
+  return hexadecimal;
+}
+
+void imprimirConversoes(List<int> numeros) {
+  numeros.sort();
+
+  for (int numero in numeros) {
+    print(
+      'decimal: $numero, '
+      'binário: ${convertBinario(numero)}, '
+      'octal: ${convertOctal(numero)}, '
+      'hexadecimal: ${convertHexadecimal(numero)}',
+    );
+  }
+}
