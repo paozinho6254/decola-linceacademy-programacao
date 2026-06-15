7763c52d28fbb5de15bfc15942137017f8217032
paozinho6254 <antoniorochadasilvaa@gmail.com>
2026-06-15 16:11:07 -0300

    Adição de mini exercícios em dart

---
+++ Formacao_dart/testes/operadorTeste.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+import 'dart:math';
+
+final aleatorio = Random();
+
+void main() {
+  final escolha = aleatorio.nextInt(3) + 1;
+  final valores = {1: 'Receba', 2: 200, 3: 7.62};
+
+  final tipo = valores[escolha];
+
+  if (tipo is String) {
+    print("String");
+  }
+  if (tipo is int) {
+    print("Int");
+  }
+  if (tipo is double) {
+    print("Double");
+  }
+  if (tipo is! num) {
+    print("Ou seja não é numero");
+  } else {
+    print("É um numero");
+  }
+
+
+
+
+}
+
+
