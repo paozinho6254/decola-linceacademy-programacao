71f34b7bdbd0243a9e9a57e1b041264be6b1231b
paozinho6254 <antoniorochadasilvaa@gmail.com>
2026-06-23 12:01:58 -0300

    Feat: adicao da atividade 4 de ap5_logica

---
+++ ap5_logica/ap4.dart
@@ -0,0 +1,35 @@
+void main() {
+  final listaNomes = [
+    "Joao",
+    "Maria",
+    "Pedro",
+    "Maria",
+    "Ana",
+    "Joao",
+    "Maria",
+    "Fernanda",
+    "Carlos",
+    "Maria",
+  ];
+
+  final nome = 'Ana';
+  final quantidade = contarNome(listaNomes, nome);
+
+  if (quantidade == 1) {
+    print('O nome $nome foi encontrado 1 vez');
+  } else if (quantidade > 0) {
+    print('O nome $nome foi encontrado $quantidade vezes');
+  } else {
+    print('O nome nao foi encontrado');
+  }
+}
+
+int contarNome(List nomes, String nome) {
+  int quantidade = 0;
+  for (var nomeIndex in nomes) {
+    if (nomeIndex == nome) {
+      quantidade++;
+    }
+  }
+  return quantidade;
+}
